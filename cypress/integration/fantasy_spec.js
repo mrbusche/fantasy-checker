b44b69dc7adb20f444368640add38224a9d18155
Matt Busche <mrbusche@gmail.com>
2021-09-05 22:43:59 -0500

    test cypress

--- cypress/integration/fantasy_spec.js
+++ cypress/integration/fantasy_spec.js
@@ -5,7 +5,7 @@ describe('find fantasy adds and drops', () => {
         cy.get('.Table__TBODY').eq(1).should(($div) => {
             const text = $div.text();
 
-            expect(text).to.eq('000000000000000000000010003102000000002112000010000000000000');
+            expect(text).to.eq('000000000000000000000010003102000000002112000010000000000001');
         })
     })
 
