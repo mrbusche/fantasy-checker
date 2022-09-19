2213d7c7e6edc487e36f850314d80849c5cc9763
Matt Busche <mrbusche@gmail.com>
2022-09-18 21:12:18 -0500

    Update fantasy.cy.js

--- cypress/e2e/fantasy.cy.js
+++ cypress/e2e/fantasy.cy.js
@@ -5,7 +5,7 @@ describe('find fantasy adds and drops', () => {
         cy.get('.Table__TBODY').eq(1).should(($div) => {
             const text = $div.text();
 
-            expect(text).to.eq('000120000330001222100341102251100010101622100060000010000121');
+            expect(text).to.eq('000130013570004522114681102251101110104922102290002210002132');
         })
     })
 
